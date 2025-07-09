05db0afb2a104ae047299d3b24edae8068c1496c
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2025-07-09 09:46:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,16 +26,16 @@ NOR_IMG = environ.get("NOR_IMG", "https://files.catbox.moe/0sxu6o.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/14ca5674072491cda2d59.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/0e2bc4c69e7a471e242b8.jpg'))
-CODE = (environ.get('CODE', 'https://t.me/+rDsRHXlP_IQ2NGFl'))
+CODE = (environ.get('CODE', 'https://t.me/Hindi_movie_uplod'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'arlinks.in'))
 STREAM_API = (environ.get('STREAM_API', 'c8deaa7cb811330e142c77d4050754c9cda60dfa'))
-STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/+rDsRHXlP_IQ2NGFl'))
+STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/Hindi_movie_uplod'))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7436687322').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002520793005').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002321973166').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '7436687322').split()]
@@ -65,7 +65,7 @@ MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Ak9u-iW4Lps5MjFl')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+rDsRHXlP_IQ2NGFl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Hindi_movie_uplod')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_downlod09/7')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : Abhikumar7799')
